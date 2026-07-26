39d73219e1e80b9e1542dc5d04ac7b6351f1562b
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-07-26 11:39:53 +0300

    add cargo test to release.sh

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo build --release --bin phdupes && ./target/release/phdupes --version && cargo release patch --no-publish --execute
+cargo build --release --bin phdupes && cargo test --release && ./target/release/phdupes --version && cargo release patch --no-publish --execute
