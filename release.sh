440adb0039940ffd8f5777239975133cd37c870f
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-08-05 07:22:33 +0300

    remove unused dep delaunator
    also, run "cargo +nightly udeps --all-targets" in release.sh

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cargo build --release --bin phdupes && cargo test --release && ./target/release/phdupes --version && cargo release patch --no-publish --execute
+cargo build --release --bin phdupes && cargo +nightly udeps --all-targets && \
+	cargo test --release && ./target/release/phdupes --version && cargo release patch --no-publish --execute
